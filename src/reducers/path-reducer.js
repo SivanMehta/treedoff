2eaa588add9bbc5d9a47cd0f8ad2dd3e63cb5bb5
Connor <cqm@andrew.cmu.edu>
2017-04-04 08:21:28 -0700

    fix typo, same commit as before

--- src/reducers/path-reducer.js
+++ src/reducers/path-reducer.js
@@ -13,7 +13,7 @@ import * as types from '../actions/action-types';
 //   };
 // };
 
-export default (path = [test], action) => {
+export default (path = [], action) => {
 	// change this to use statement not pro and index
   switch (action.type) {
     case types.ADVANCE_PATH:
