b77b3a684101b3cef5ac68f21e85e2d68e664564
Sivan Mehta <Sivanmehta2@gmail.com>
2017-04-18 21:44:39 -0400

    moved static asset definition to initializer

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-const path = require('path')
-const passport = require('passport')
 const async = require('async')
 
 // setup initializers
@@ -22,33 +20,14 @@ const initializers = [
   './auth',
 
   // api definitions
-  './api'
+  './api',
+
+  // Serve static assets
+  './initializers/static-assets.js'
 
 ].map(filename => done => require(filename).init(app, done))
 
 async.waterfall(initializers, (err, _) => {
-
-  // Serve static assets
-  app.use(express.static(path.resolve(__dirname, '..', 'public')))
-
-  // check for react files
-  const fs = require('fs')
-  fs.access(path.resolve(__dirname, '..', 'public', 'build'), fs.constants.F_OK | fs.constants.R_OK, (err) => {
-    if(err) {
-      const suggestion = " --> The React.js application was probably not built"
-      throw err + suggestion
-    }
-  })
-
-  // D3 vis routes
-  app.get('/d3', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'd3.html'))
-  })
-
-  // Always return the main index.html, so react-router render the route in the client
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))
-  })
 
   module.exports = app
 
---
+++ server/initializers/static-assets.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const fs = require('fs')
+const express = require('express')
+
+exports.init = (app, done) => {
+  // Serve static assets
+  app.use(express.static(path.resolve(__dirname, '..', '..', 'public')))
+
+  // check for react files
+  fs.access(path.resolve(__dirname, '..', '..', 'public', 'build'), fs.constants.F_OK | fs.constants.R_OK, (err) => {
+    if(err) {
+      const suggestion = " --> The React.js application was probably not built"
+      throw err + suggestion
+    }
+  })
+
+  // D3 vis routes
+  app.get('/d3', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'd3.html'))
+  })
+
+  // Always return the main index.html, so react-router render the route in the client
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '..', '..', 'public', 'index.html'))
+  })
+
+  app.log.info('Static assets, including the React app, are available')
+  done()
+}
